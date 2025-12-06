315e1d9fb1b754cfaa835e60315eef1cccd4b656
Iversonweb <iversonweb98@gmail.com>
2025-12-06 01:31:04 +0100

    chore: some ui updates

--- patterns/sections/explore-categories.php
+++ patterns/sections/explore-categories.php
@@ -24,5 +24,221 @@
         <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
+
+    <!-- wp:group {"className":"container","layout":{"type":"constrained"}} -->
+    <div class="wp-block-group container">
+        <!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|60"}}},"layout":{"type":"grid","minimumColumnWidth":"16rem"}} -->
+        <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--60)"><!-- wp:columns -->
+            <div class="wp-block-columns">
+                <!-- wp:column {"style":{"spacing":{"padding":{"top":"40px","bottom":"40px","left":"20px","right":"20px"}},"border":{"radius":{"topLeft":"16px","topRight":"16px","bottomLeft":"16px","bottomRight":"16px"}}},"backgroundColor":"white"} -->
+                <div class="wp-block-column has-white-background-color has-background"
+                    style="border-top-left-radius:16px;border-top-right-radius:16px;border-bottom-left-radius:16px;border-bottom-right-radius:16px;padding-top:40px;padding-right:20px;padding-bottom:40px;padding-left:20px">
+                    <!-- wp:image {"id":143,"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full is-resized"><img
+                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/file-invoice.png" alt=""
+                            class="wp-image-143" style="width:40px" /></figure>
+                    <!-- /wp:image -->
+
+                    <!-- wp:heading {"textAlign":"left","level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|jr-paleblue"}}}},"textColor":"jr-paleblue","fontFamily":"jr-jost"} -->
+                    <h5
+                        class="wp-block-heading has-text-align-left has-jr-paleblue-color has-text-color has-link-color has-jr-jost-font-family">
+                        <strong>Accounting &amp; Finance</strong></h5>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph {"align":"left","style":{"spacing":{"margin":{"top":"8px"}},"typography":{"fontSize":"14px"}},"fontFamily":"jr-jost"} -->
+                    <p class="has-text-align-left has-jr-jost-font-family" style="margin-top:8px;font-size:14px">You can
+                        view all popular jobs according your future careers.</p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+
+            <!-- wp:columns -->
+            <div class="wp-block-columns">
+                <!-- wp:column {"style":{"spacing":{"padding":{"top":"40px","bottom":"40px","left":"20px","right":"20px"}},"border":{"radius":{"topLeft":"16px","topRight":"16px","bottomLeft":"16px","bottomRight":"16px"}}},"backgroundColor":"white"} -->
+                <div class="wp-block-column has-white-background-color has-background"
+                    style="border-top-left-radius:16px;border-top-right-radius:16px;border-bottom-left-radius:16px;border-bottom-right-radius:16px;padding-top:40px;padding-right:20px;padding-bottom:40px;padding-left:20px">
+                    <!-- wp:image {"id":144,"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full is-resized"><img
+                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/bus-alt.png" alt="" class="wp-image-144"
+                            style="width:40px" /></figure>
+                    <!-- /wp:image -->
+
+                    <!-- wp:heading {"textAlign":"left","level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|jr-paleblue"}}}},"textColor":"jr-paleblue","fontFamily":"jr-jost"} -->
+                    <h5
+                        class="wp-block-heading has-text-align-left has-jr-paleblue-color has-text-color has-link-color has-jr-jost-font-family">
+                        <strong>Automotive Jobs</strong></h5>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph {"align":"left","style":{"spacing":{"margin":{"top":"8px"}},"typography":{"fontSize":"14px"}},"fontFamily":"jr-jost"} -->
+                    <p class="has-text-align-left has-jr-jost-font-family" style="margin-top:8px;font-size:14px">You can
+                        view all popular jobs according your future careers.</p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+
+            <!-- wp:columns -->
+            <div class="wp-block-columns">
+                <!-- wp:column {"style":{"spacing":{"padding":{"top":"40px","bottom":"40px","left":"20px","right":"20px"}},"border":{"radius":{"topLeft":"16px","topRight":"16px","bottomLeft":"16px","bottomRight":"16px"}}},"backgroundColor":"white"} -->
+                <div class="wp-block-column has-white-background-color has-background"
+                    style="border-top-left-radius:16px;border-top-right-radius:16px;border-bottom-left-radius:16px;border-bottom-right-radius:16px;padding-top:40px;padding-right:20px;padding-bottom:40px;padding-left:20px">
+                    <!-- wp:image {"id":145,"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full is-resized"><img
+                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/smart-building.png" alt=""
+                            class="wp-image-145" style="width:40px" /></figure>
+                    <!-- /wp:image -->
+
+                    <!-- wp:heading {"textAlign":"left","level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|jr-paleblue"}}}},"textColor":"jr-paleblue","fontFamily":"jr-jost"} -->
+                    <h5
+                        class="wp-block-heading has-text-align-left has-jr-paleblue-color has-text-color has-link-color has-jr-jost-font-family">
+                        <strong>Business &amp; Tech</strong></h5>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph {"align":"left","style":{"spacing":{"margin":{"top":"8px"}},"typography":{"fontSize":"14px"}},"fontFamily":"jr-jost"} -->
+                    <p class="has-text-align-left has-jr-jost-font-family" style="margin-top:8px;font-size:14px">You can
+                        view all popular jobs according your future careers.</p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+
+            <!-- wp:columns -->
+            <div class="wp-block-columns">
+                <!-- wp:column {"style":{"spacing":{"padding":{"top":"40px","bottom":"40px","left":"20px","right":"20px"}},"border":{"radius":{"topLeft":"16px","topRight":"16px","bottomLeft":"16px","bottomRight":"16px"}}},"backgroundColor":"white"} -->
+                <div class="wp-block-column has-white-background-color has-background"
+                    style="border-top-left-radius:16px;border-top-right-radius:16px;border-bottom-left-radius:16px;border-bottom-right-radius:16px;padding-top:40px;padding-right:20px;padding-bottom:40px;padding-left:20px">
+                    <!-- wp:image {"id":146,"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full is-resized"><img
+                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/student-alt.png" alt=""
+                            class="wp-image-146" style="width:40px" /></figure>
+                    <!-- /wp:image -->
+
+                    <!-- wp:heading {"textAlign":"left","level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|jr-paleblue"}}}},"textColor":"jr-paleblue","fontFamily":"jr-jost"} -->
+                    <h5
+                        class="wp-block-heading has-text-align-left has-jr-paleblue-color has-text-color has-link-color has-jr-jost-font-family">
+                        <strong>Education Training</strong></h5>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph {"align":"left","style":{"spacing":{"margin":{"top":"8px"}},"typography":{"fontSize":"14px"}},"fontFamily":"jr-jost"} -->
+                    <p class="has-text-align-left has-jr-jost-font-family" style="margin-top:8px;font-size:14px">You can
+                        view all popular jobs according your future careers.</p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+
+            <!-- wp:columns -->
+            <div class="wp-block-columns">
+                <!-- wp:column {"style":{"spacing":{"padding":{"top":"40px","bottom":"40px","left":"20px","right":"20px"}},"border":{"radius":{"topLeft":"16px","topRight":"16px","bottomLeft":"16px","bottomRight":"16px"}}},"backgroundColor":"white"} -->
+                <div class="wp-block-column has-white-background-color has-background"
+                    style="border-top-left-radius:16px;border-top-right-radius:16px;border-bottom-left-radius:16px;border-bottom-right-radius:16px;padding-top:40px;padding-right:20px;padding-bottom:40px;padding-left:20px">
+                    <!-- wp:image {"id":147,"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full is-resized"><img
+                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/first-aid-kit.png" alt=""
+                            class="wp-image-147" style="width:40px" /></figure>
+                    <!-- /wp:image -->
+
+                    <!-- wp:heading {"textAlign":"left","level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|jr-paleblue"}}}},"textColor":"jr-paleblue","fontFamily":"jr-jost"} -->
+                    <h5
+                        class="wp-block-heading has-text-align-left has-jr-paleblue-color has-text-color has-link-color has-jr-jost-font-family">
+                        <strong>Healthcare</strong></h5>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph {"align":"left","style":{"spacing":{"margin":{"top":"8px"}},"typography":{"fontSize":"14px"}},"fontFamily":"jr-jost"} -->
+                    <p class="has-text-align-left has-jr-jost-font-family" style="margin-top:8px;font-size:14px">You can
+                        view all popular jobs according your future careers.</p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+
+            <!-- wp:columns -->
+            <div class="wp-block-columns">
+                <!-- wp:column {"style":{"spacing":{"padding":{"top":"40px","bottom":"40px","left":"20px","right":"20px"}},"border":{"radius":{"topLeft":"16px","topRight":"16px","bottomLeft":"16px","bottomRight":"16px"}}},"backgroundColor":"white"} -->
+                <div class="wp-block-column has-white-background-color has-background"
+                    style="border-top-left-radius:16px;border-top-right-radius:16px;border-bottom-left-radius:16px;border-bottom-right-radius:16px;padding-top:40px;padding-right:20px;padding-bottom:40px;padding-left:20px">
+                    <!-- wp:image {"id":148,"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full is-resized"><img
+                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/burger-fries.png" alt=""
+                            class="wp-image-148" style="width:40px" /></figure>
+                    <!-- /wp:image -->
+
+                    <!-- wp:heading {"textAlign":"left","level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|jr-paleblue"}}}},"textColor":"jr-paleblue","fontFamily":"jr-jost"} -->
+                    <h5
+                        class="wp-block-heading has-text-align-left has-jr-paleblue-color has-text-color has-link-color has-jr-jost-font-family">
+                        <strong>Restaurant &amp; Food</strong></h5>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph {"align":"left","style":{"spacing":{"margin":{"top":"8px"}},"typography":{"fontSize":"14px"}},"fontFamily":"jr-jost"} -->
+                    <p class="has-text-align-left has-jr-jost-font-family" style="margin-top:8px;font-size:14px">You can
+                        view all popular jobs according your future careers.</p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+
+            <!-- wp:columns -->
+            <div class="wp-block-columns">
+                <!-- wp:column {"style":{"spacing":{"padding":{"top":"40px","bottom":"40px","left":"20px","right":"20px"}},"border":{"radius":{"topLeft":"16px","topRight":"16px","bottomLeft":"16px","bottomRight":"16px"}}},"backgroundColor":"white"} -->
+                <div class="wp-block-column has-white-background-color has-background"
+                    style="border-top-left-radius:16px;border-top-right-radius:16px;border-bottom-left-radius:16px;border-bottom-right-radius:16px;padding-top:40px;padding-right:20px;padding-bottom:40px;padding-left:20px">
+                    <!-- wp:image {"id":149,"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full is-resized"><img
+                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/plane-globe.png" alt=""
+                            class="wp-image-149" style="width:40px" /></figure>
+                    <!-- /wp:image -->
+
+                    <!-- wp:heading {"textAlign":"left","level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|jr-paleblue"}}}},"textColor":"jr-paleblue","fontFamily":"jr-jost"} -->
+                    <h5
+                        class="wp-block-heading has-text-align-left has-jr-paleblue-color has-text-color has-link-color has-jr-jost-font-family">
+                        <strong>Transportation</strong></h5>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph {"align":"left","style":{"spacing":{"margin":{"top":"8px"}},"typography":{"fontSize":"14px"}},"fontFamily":"jr-jost"} -->
+                    <p class="has-text-align-left has-jr-jost-font-family" style="margin-top:8px;font-size:14px">You can
+                        view all popular jobs according your future careers.</p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+
+            <!-- wp:columns -->
+            <div class="wp-block-columns">
+                <!-- wp:column {"style":{"spacing":{"padding":{"top":"40px","bottom":"40px","left":"20px","right":"20px"}},"border":{"radius":{"topLeft":"16px","topRight":"16px","bottomLeft":"16px","bottomRight":"16px"}}},"backgroundColor":"white"} -->
+                <div class="wp-block-column has-white-background-color has-background"
+                    style="border-top-left-radius:16px;border-top-right-radius:16px;border-bottom-left-radius:16px;border-bottom-right-radius:16px;padding-top:40px;padding-right:20px;padding-bottom:40px;padding-left:20px">
+                    <!-- wp:image {"id":150,"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full is-resized"><img
+                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/mobile-notch.png" alt=""
+                            class="wp-image-150" style="width:40px" /></figure>
+                    <!-- /wp:image -->
+
+                    <!-- wp:heading {"textAlign":"left","level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|jr-paleblue"}}}},"textColor":"jr-paleblue","fontFamily":"jr-jost"} -->
+                    <h5
+                        class="wp-block-heading has-text-align-left has-jr-paleblue-color has-text-color has-link-color has-jr-jost-font-family">
+                        <strong>Telecommunications</strong></h5>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph {"align":"left","style":{"spacing":{"margin":{"top":"8px"}},"typography":{"fontSize":"14px"}},"fontFamily":"jr-jost"} -->
+                    <p class="has-text-align-left has-jr-jost-font-family" style="margin-top:8px;font-size:14px">You can
+                        view all popular jobs according your future careers.</p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
 </section>
 <!-- /wp:group -->
